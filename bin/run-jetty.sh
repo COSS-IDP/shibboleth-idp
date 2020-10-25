3f3eee675c5951be40543062d829623b8686626c
Ryu, Gyoung Seok <narzis@gmail.com>
2020-10-26 07:24:45 +0900

    save

--- bin/run-jetty.sh
+++ bin/run-jetty.sh
@@ -2,7 +2,7 @@
 
 #set -x
 
-export JAVA_HOME=/opt/jre-home
+export JAVA_HOME=/usr/lib/jvm/default-jvm \
 export PATH=$PATH:$JAVA_HOME/bin
 
 if [ -e "/opt/shibboleth-idp/ext-conf/idp-secrets.properties" ]; then
@@ -10,7 +10,6 @@ if [ -e "/opt/shibboleth-idp/ext-conf/idp-secrets.properties" ]; then
   export JETTY_BROWSER_SSL_KEYSTORE_PASSWORD=`gawk 'match($0,/^jetty\.sslContext\.keyStorePassword=\s?(.*)\s?$/, a) {print a[1]}' /opt/shibboleth-idp/ext-conf/idp-secrets.properties`
 fi
 
-export JETTY_ARGS="jetty.sslContext.keyStorePassword=$JETTY_BROWSER_SSL_KEYSTORE_PASSWORD jetty.backchannel.sslContext.keyStorePassword=$JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD"
-sed -i "s/^-Xmx.*$/-Xmx$JETTY_MAX_HEAP/g" /opt/shib-jetty-base/start.ini
+# export JETTY_ARGS="jetty.sslContext.keyStorePassword=58463 jetty.backchannel.sslContext.keyStorePassword=$JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD"
 
 exec /opt/jetty-home/bin/jetty.sh run
